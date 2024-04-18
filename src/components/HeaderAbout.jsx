62e31d689fe3de024a3e5715fe0d019fe8b3cdb9
firkik <161239495+firkik@users.noreply.github.com>
2024-04-18 23:38:02 +0500

    Delete src directory

--- src/components/HeaderAbout.jsx
+++
@@ -1,9 +0,0 @@
-import Arrow from '../assets/arrow.png'
-
-export default function HeaderAbout() {
-    return (
-        <div className="last_str">
-            <a href="./index.html"><img src={Arrow} alt="arrow" /></a>
-        </div>
-    )
-}
