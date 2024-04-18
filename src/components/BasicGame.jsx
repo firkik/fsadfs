df44c261bc2a8ffd3a60e17aa334c26a2828535b
Pehhenika <157368481+Pehhenika@users.noreply.github.com>
2024-04-18 20:36:47 +0500

    Add files via upload

---
+++ src/components/BasicGame.jsx
@@ -0,0 +1,16 @@
+import Cookie from '../assets/cookie.png'
+
+export default function BasicGame() {
+  return (
+    <div className="basic-game-container">
+      <div className="basic-game-text">
+        <h2>The basics of the game</h2>
+        <p>
+          {" "}
+          Go into the game every day and bite a<br/>cookie, thereby you will get
+          more in<br/>the end. You can also complete tasks<br/>and invite friends.
+        </p>
+      </div>
+    </div>
+  );
+}
